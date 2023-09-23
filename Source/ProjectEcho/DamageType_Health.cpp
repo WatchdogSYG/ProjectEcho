c8781dd7042d5b834efff82c66d421788232ac3c
Watchdog <brandonlu001@gmail.com>
2023-09-23 12:37:07 +0930

    Removed Deprecated Assets and classes

--- Source/ProjectEcho/DamageType_Health.cpp
+++
@@ -1,5 +0,0 @@
-// This is a prototype of a game, currently for non-commercial purposes. This may change at any time without notice in the future and may be licenced under different conditions. You may not copy or redistribute content for commercial purposes. TODO: Assign licence (CC or otherwise).
-
-
-#include "DamageType_Health.h"
-
